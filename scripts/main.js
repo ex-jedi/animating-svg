d5fa2f243d62bd0ce7d2887d9cda38b0af86b479
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-11-29 16:03:12 +0000

    Longer delay

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ gsap.to('.element', {
   duration: 3,
   backgroundColor: '#00f',
   opacity: 1,
-  delay: 2,
-  stagger: () => Math.random(),
+  delay: 0.5,
+  stagger: () => Math.random() * 1,
   ease: 'ease',
 });
