f07bcb1ec0e7ebd7cb4ba697d50bea0cb22a4569
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-11-29 11:28:23 +0000

    Circle bounce with CSS property changing

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-TweenMax.staggerTo('.group div', 1, { x: 500 }, -0.5);
+gsap.to('.element', {
+  duration: 2,
+  y: 500,
+  backgroundColor: '#00f',
+  borderRadius: 0,
+  delay: 0.5,
+  stagger: 0.1,
+  ease: 'bounce',
+});
