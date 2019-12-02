2df5a0d224fd85a2fd7372a6974dd7ba1193e3a6
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-02 18:48:09 +0000

    First ScrollMagic scene and removing jQuery

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-$(document).ready(function(){
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
 
-	
+// Initialising ScrollMagic controller
+const controller = new ScrollMagic.Controller();
 
+const ourScene = new ScrollMagic.Scene({
+  triggerElement: '#project01',
 });
+ourScene
+  .setClassToggle('#project01', 'fade-in')
+  .addIndicators({
+    name: 'fade scene',
+    colorTrigger: '#000',
+    colorStart: '#0000aa',
+  }) //! Needs Plugin
+  .addTo(controller);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//* Add .reverse(false) to prevent class toggling off.
